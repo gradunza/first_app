91e5fa8a0bcfe15f3fb023846b7603c4ee95a9fe
Asia Pacific <apac@APACMOBILE.(none)>
2011-11-19 19:09:14 -0500

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3159e709b580c87e678c13e6f0c54493bf22b5d0c5b5898a3f4c995c136b2143250f0a89662591e012102fb8e54951f1ddafb257b1e78b22d181cdfe7b788ccb'
